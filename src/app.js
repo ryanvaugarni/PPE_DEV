f8f3fb70935438ddf2f8b8e7f06152d237cb9379
Vaugarni ryan <ryan.vaugarni@gmail.com>
2022-03-31 17:37:04 +0200

    update js

--- src/app.js
+++ src/app.js
@@ -34,4 +34,38 @@ document.addEventListener("DOMContentLoaded", e => {
     closed_all.addEventListener("click", () => {
         modal.classList.remove("modale-active");
     });
+    /*FORM*/
+    let el2 = document.querySelector("form p");
+	let btn, nom, prenom, email, pays, ville;
+	btn = document.getElementsByTagName("input")[3];
+	nom = document.getElementsByTagName("input")[0].value;
+	prenom = document.getElementsByTagName("input")[1].value;
+	email = document.getElementsByTagName("input")[2].value;
+	pays = document.getElementsByTagName("select")[0].value;
+	ville = document.getElementsByTagName("select")[1].value;
+	btn.addEventListener("click", e => {
+		e.preventDefault();
+		Formulaire();
+	});
+	let Formulaire = () => {
+		btn = document.getElementsByTagName("input")[3];
+		nom = document.getElementsByTagName("input")[0].value;
+		prenom = document.getElementsByTagName("input")[1].value;
+		email = document.getElementsByTagName("input")[2].value;
+		pays = document.getElementsByTagName("select")[0].value;
+		ville = document.getElementsByTagName("select")[1].value;
+		console.log(nom, prenom, email, pays, ville);
+		if (nom === "" || prenom === "" || email === "" || pays === "" || ville === "") {
+			el2.innerText = "Veuillez remplir tous les champs ";
+			el2.classList.remove("success");
+			el2.classList.add("warning");
+		} else {
+			el2.innerText = "Votre formulaire a bien été envoyé";
+			el2.classList.remove("warning");
+			el2.classList.add("success");
+			localStorage.setItem("user", nom + " | " + prenom + " | " + email + " | " + pays + " | " + ville);
+			sessionStorage.setItem("user", "sessionid");
+
+		}
+	};
 });
